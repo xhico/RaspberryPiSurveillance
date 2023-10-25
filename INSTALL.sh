bc99fb231dfeab19cefd389dbdf915fce6504cf9
xhico <francgf@gmail.com>
2023-10-25 16:10:22 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,5 +2,6 @@
 
 sudo mv /home/pi/RaspberryPiSurveillance/RaspberryPiSurveillance.service /etc/systemd/system/ && sudo systemctl daemon-reload
 python3 -m pip install yagmail moviepy gpiozero picamera2 --no-cache-dir
+sudo apt install python3-numpy python3-kms++ -y
 mkdir -p /home/pi/RaspberryPiSurveillance/_RECORDINGS
 chmod +x -R /home/pi/RaspberryPiSurveillance/*
