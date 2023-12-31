c5a53dc5456ec432a5806810deba0ef8747f167a
xhico <francgf@gmail.com>
2023-12-31 16:00:55 +0000

    Add requirements.txt

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo mv /home/pi/RaspberryPiSurveillance/RaspberryPiSurveillance.service /etc/systemd/system/ && sudo systemctl daemon-reload
-python3 -m pip install yagmail moviepy gpiozero picamera2 --no-cache-dir
+python3 -m pip install -r /home/pi/RaspberryPiSurveillance/requirements.txt --no-cache-dir
 sudo apt install python3-numpy python3-kms++ sense-hat python3-libcamera -y
 mkdir -p /home/pi/RaspberryPiSurveillance/_RECORDINGS
 chmod +x -R /home/pi/RaspberryPiSurveillance/*
